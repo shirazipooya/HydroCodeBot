8127c7886b22096ccc6fc559540aedaf08a7b58f
Pooya Shirazi <p.shirazi.a@gmail.com>
2024-12-15 22:33:10 +0330

    ...

--- app.py
+++ app.py
@@ -861,8 +861,8 @@ async def main():
     scheduler.add_job(
         func=report_user_count,
         trigger='cron',
-        hour='0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23',
-        minute='0, 30',
+        hour='0, 6, 12, 18',
+        minute='30',
         second=0
     )
     scheduler.start()    
